84501a8c840930e255fdbcc2b299182906fb5b22
Rahul Bansal <rahul.bansal@rtcamp.com>
2012-03-21 19:05:30 +0530

    cleanup

--- 0.2/js/custom-ie6.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-    /* Current Menu Class */
-	jQuery("li.current-menu-item > a").addClass("current-menu");
-
-    /* Admin Comment Class */
-	jQuery("li.comment-author-admin > div.comment-body").addClass("admin-comment");
-});
